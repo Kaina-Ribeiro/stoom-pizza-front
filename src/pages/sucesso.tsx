d9a3ddf60ecf4968e114121bcce385a065922834
Gabriel Ribeiro <gabrielkaina99@gmail.com>
2021-05-23 18:43:50 -0300

    all pages added

---
+++ src/pages/sucesso.tsx
@@ -0,0 +1,16 @@
+
+const Sucesso = () => {
+  // redireciona para página inicial se não validar etapa-3
+
+  return (
+    <>
+      <h1></h1>
+
+      <div>
+
+      </div>
+    </>
+  )
+}
+
+export default Sucesso
